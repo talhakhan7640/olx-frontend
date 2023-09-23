908484495aeb2b204525410b3180a17014319a64
talhakhan7640 <talhakhan7640@gmail.com>
2023-09-24 00:31:47 +0530

    added working routes on each category

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,22 +1,42 @@
 import React from 'react'
 import '../assets/styles/Categories.css'
+import { Link } from 'react-router-dom'
 
 const Categories = () => {
   return (
     <div className='categories-container py-4'>
         <div className="flex justify-around">
             {/* <span>Home</span> */}
-            <span>Tvs</span>
-            <span>Laptops & Computers</span>
-            <span>Beds</span>
+            <span>
+              <Link to="/crumpled/categories/tvs" className="">
+                TVs
+                  </Link>
+            </span>
+            <span>
+              Laptops & Computers</span>
+            <span>
+
+              <Link to="/crumpled/categories/beds" className="">
+                Beds
+                  </Link>
+
+            </span>
             <span>Sofas</span>
             <span>Dining</span>
             <span>Washing Machine</span>
             <span>Cars</span>
             <span>Bikes</span>
-            <span>Motorcycles</span>
+            <span>
+              <Link to="/crumpled/categories/motorcycles" className="">
+                Motorcycles
+                  </Link>
+            </span>
             <span>Sports Equipments</span>
-            <span>Mobiles</span>
+            <span>
+              <Link to="/crumpled/categories/mobiles" className="">
+                Mobiles
+                  </Link>
+            </span>
         </div>
         <div className='divider my-3'></div>
     </div>
