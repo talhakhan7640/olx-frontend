a7d441f9442dc5277c0606deeefcb8a51c1689e6
talhakhan7640 <talhakhan7640@gmail.com>
2023-09-28 13:09:50 +0530

    made a few changes

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,43 +1,55 @@
-import React from 'react'
-import '../assets/styles/Sidebar.css'
-import { Link } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom'
-import Cookies from 'universal-cookie'
+import React from "react";
+import "../assets/styles/Sidebar.css";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import Cookies from "universal-cookie";
+import { useSelector } from "react-redux";
 
 const Sidebar = () => {
-  const cookies = new Cookies()
-  const navigate = useNavigate()
+  const user = useSelector((state) => state.authenticatedUser.value);
+
+  const cookies = new Cookies();
+  const navigate = useNavigate();
   const logOutUser = (e) => {
-    e.preventDefault()
-    cookies.remove("TOKEN", {path: "/"});
-    navigate('/crumpled/users/login')
-  }
+    e.preventDefault();
+    cookies.remove("TOKEN", { path: "/" });
+    navigate("/crumpled/users/login");
+  };
   return (
     <div className="sidebar-container">
-      <ul className="sidebar-items-container py-2 px-12">
-        <li className="sidebar-item py-2 px-4 mx-2 my-">
-          <Link to="/crumpled/home" className="">
+      <ul className="sidebar-items-container px-2 py-2 lg:px-12">
+        <li className="sidebar-item py-2 px-4 mx-1 my- mb-8">
+          <span className="authUser text-xl">Welcome {user}</span>
+        </li>
+        <li className="sidebar-item py-2 px-4 mx-1 my-2">
+          <Link to="/crumpled/home" className="text-lg">
             Home
-                  </Link>
+          </Link>
         </li>
-        <li className="sidebar-item py-2  px-4 mx-2 my-2">
-          <Link to="/crumpled/messages" className="">
+        <li className="sidebar-item py-2  px-4 mx-1 my-3">
+          <Link to="/crumpled/messages" className="text-lg">
             Messages
-                  </Link>
+          </Link>
         </li>
-        <li className="sidebar-item py-2 px-4 mx-2 my-2">
-          <Link to="/crumpled/favorite" className="">
+        <li className="sidebar-item py-2 px-4 mx-1 my-3">
+          <Link to="/crumpled/favorite" className="text-lg">
             Favorite
-                  </Link> </li>
-        <li className="sidebar-item py-2 px-4 mx-2 my-2"> <Link to="/crumpled/profile" className="">
-           Profile 
-                  </Link></li>
-        <li className="sidebar-item py-2 px-4 mx-2 my-2">
-          <button onClick={logOutUser}>Logout</button>
+          </Link>{" "}
+        </li>
+        <li className="sidebar-item py-2 px-4 mx-1 my-3">
+          {" "}
+          <Link to="/crumpled/profile" className="text-lg">
+            Profile
+          </Link>
+        </li>
+        <li className="sidebar-item py-2 px-4 mx-1 my-3">
+          <button onClick={logOutUser} className="text-lg">
+            Logout
+          </button>
         </li>
       </ul>
     </div>
   );
-}
+};
 
-export default Sidebar
+export default Sidebar;
