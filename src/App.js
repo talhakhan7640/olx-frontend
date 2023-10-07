2ff5558e6d2f6277a060abdb11b9ed65437b50c9
talhakhan7640 <talhakhan7640@gmail.com>
2023-10-07 20:05:25 +0530

    Changed the layout of application

--- src/App.js
+++ src/App.js
@@ -1,33 +1,31 @@
 import React from "react";
 // import Login from './components/Login.jsx';
-import './assets/styles/Home.css'
+import "./assets/styles/Home.css";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import Categories from "./components/Categories";
 
-import Navbar from './components/Navbar'
-import Sidebar from './components/Sidebar'
-import Categories from './components/Categories'
-
-function App({children}) {
+function App({ children }) {
   return (
-    <div className='App'>
-    <div className='navbar-container py-4'>
+    <div className="App">
+      <div className="navbar-container py-4">
         <Navbar />
-    </div>
-   
-    <div className="sidebar-and-items-container  container mx-auto">
-      <div className='grid grid-cols-9 mx-2 lg:mx-12'>
+      </div>
 
-      <div className='sidebar-conatainer col-span-2 lg:col-span-2 xl:col-span-2'>
-        <Sidebar />
+      <div className="sidebar-and-items-container  container mx-auto">
+        <div className="grid grid-cols-9 mx-2 lg:mx-12">
+          <div className="sidebar-conatainer col-span-2 lg:col-span-2 xl:col-span-2">
+            <Sidebar />
+          </div>
+          <div className="items-container col-span-7 lg:col-span-7  xl:col-span-7">
+            <div className="categories-container">
+              <Categories />
+            </div>
+            {children}
+          </div>
+        </div>
       </div>
-      <div className='items-container col-span-7 lg:col-span-7  xl:col-span-7'>
-         <div className="categories-container">
-      <Categories />
     </div>
-        {children}
-      </div>
-      </div>
-    </div>
-</div>
   );
 }
 
